1fa3655ff16a01e241ca4b7375837cba1caa3182
Johan Vos <johan@lodgon.com>
2019-05-29 21:33:01 +0200

    make JNI_OnLoad_pango

--- modules/javafx.graphics/src/main/native-font/pango.c
+++ modules/javafx.graphics/src/main/native-font/pango.c
@@ -32,10 +32,26 @@
 #include <pango/pangoft2.h>
 #include <dlfcn.h>
 
+JNIEXPORT jint JNICALL
+JNI_OnLoad_javafx_font_pango(JavaVM * vm, void * reserved) {
+#ifdef JNI_VERSION_1_8
+    //min. returned JNI_VERSION required by JDK8 for builtin libraries
+    JNIEnv *env;
+    if ((*vm)->GetEnv(vm, (void **)&env, JNI_VERSION_1_8) != JNI_OK) {
+        return JNI_VERSION_1_4;
+    }
+    return JNI_VERSION_1_8;
+#else
+    return JNI_VERSION_1_4;
+#endif
+}
+
+
 #define OS_NATIVE(func) Java_com_sun_javafx_font_freetype_OSPango_##func
 
 extern jboolean checkAndClearException(JNIEnv *env);
 
+/*
 jboolean checkAndClearException(JNIEnv *env)
 {
     jthrowable t = (*env)->ExceptionOccurred(env);
@@ -45,6 +61,7 @@ jboolean checkAndClearException(JNIEnv *env)
     (*env)->ExceptionClear(env);
     return JNI_TRUE;
 }
+*/
 
 /**************************************************************************/
 /*                                                                        */
