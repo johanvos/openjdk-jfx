50248285e118bc7da7f1942deaa5ee473dc34136
Johan Vos <johan@lodgon.com>
2019-11-18 19:16:06 +0100

    add onload method and remove duplicate code

--- modules/javafx.graphics/src/main/native-glass/monocle/EGL.c
+++ modules/javafx.graphics/src/main/native-glass/monocle/EGL.c
@@ -38,7 +38,24 @@
 #include "Monocle.h"
 
 #include <stdlib.h>
+//Builtin library entrypoint
+JNIEXPORT jint JNICALL
+JNI_OnLoad_glass_monocle(JavaVM *vm, void * reserved) {
+#ifdef JNI_VERSION_1_8
+    //min. returned JNI_VERSION required by JDK8 for builtin libraries
+    JNIEnv *env;
+    if ((*vm)->GetEnv(vm, (void **)&env, JNI_VERSION_1_8) != JNI_OK) {
+        return JNI_VERSION_1_4;
+    }
+    return JNI_VERSION_1_8;
+#else
+    return JNI_VERSION_1_4;
+#endif
+}
 
+extern int setEGLAttrs(jint *attrs, int *eglAttrs);
+
+/*
 int setEGLAttrs(jint *attrs, int *eglAttrs) {
     int index = 0;
 
@@ -78,6 +95,7 @@ int setEGLAttrs(jint *attrs, int *eglAttrs) {
     eglAttrs[index] = EGL_NONE;
 return index;
 }
+*/
 
 JNIEXPORT void testGraalGL(ANativeWindow *mywindow);
 
